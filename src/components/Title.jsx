8062a889678e0685a5cf2dab9aa773a00ce91635
Kevin Septian <kevinseptian@192.168.1.17>
2025-02-26 16:06:58 +0700

    Fixing form font size

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,20 +3,20 @@
     return (
         <>
         <div className="flex flex-col items-center justify-center p-8">
-            <h1 className="text-white text-4xl md:text-5xl font-bold text-center mb-4">
+            <h1 className="text-white text-2xl md:text-5xl font-bold text-center mb-4">
             MALAM INI!
             <br />
             GRATIS Umrah & Haji 2026
             </h1>
 
             {/* Subheading */}
-            <h4 className="text-white text-xl md:text-2xl text-center mb-6">
+            <h4 className="text-white text-l md:text-2xl text-center mb-6">
             💰 Cukup menabung cerdas Rp10.000/hari
             </h4>
 
             {/* Highlighted Section */}
             <div className="bg-yellow-500 p-4 rounded-lg shadow-lg mb-6">
-            <h1 className="text-white text-3xl md:text-4xl font-bold text-center">
+            <h1 className="text-white text-l md:text-4xl font-bold text-center">
                 WAJIB HADIR!
                 <br />
                 ZOOMINAR PROGRAM PERCEPATAN
